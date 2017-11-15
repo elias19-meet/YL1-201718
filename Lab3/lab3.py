da515822456148579824942e4d131ae9ddcbc659
elias19-meet <elias19@meet.mit.edu>
2017-11-15 18:41:40 +0200

    lab1=> lab 4

---
+++ Lab3/lab3.py
@@ -0,0 +1,30 @@
+
+
+import turtle
+
+turtle.addshape("rocket.gif")
+turtle.shape("rocket.gif")
+turtle.speed(0)
+##for i in range (10000):
+##    turtle.forward(200)
+##    turtle.right(50)
+##    turtle.forward(70)
+##    turtle.right(70)
+##    turtle.forward(70)
+##    turtle.left(1)
+
+
+
+#turtle. stamp()
+
+
+star = turtle.Turtle()
+
+for i in range(5):
+    star.forward(50)
+    star.right(144)
+    
+turtle.done()
+
+    
+
